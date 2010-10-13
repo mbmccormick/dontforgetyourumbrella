66e05deee7f4e02c54e9f2117436f7688a8df940
Matt McCormick <mbmccormick@gmail.com>
2010-10-13 10:37:23 -0700

    Removed dependency on core services.

--- success.php
+++ success.php
@@ -1,18 +1,14 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
 <head>
-    <title>dontforgetyourumbrella.com</title>
+    <title>Don't Forget Your Umbrella!</title>
     <link rel="stylesheet" href="/inc/stylesheet.css" />
     <script type="text/javascript" src="/inc/util-functions.js"></script>
     <script type="text/javascript" src="/inc/clear-default-text.js"></script>
     <meta http-equiv="refresh" content="5; url=http://dontforgetyourumbrella.com/" />
 <body>
-    <?php
-        
-        include("service.php");
-        success();
-        
-    ?>
+    <span class='forecast'>Hooray. Your settings have been saved. </span>
+    <span class='umbrella'>You will receive a text message when you need your <span class='yes'>umbrella</span>.</span>
     <br />
     <br />
     <p>Copyright &copy; 2010 <a style="text-decoration: none;" href="http://www.mccormicktechnologies.com/" target="_blank">McCormick Technologies LLC</a>. All rights reserved.</p>
