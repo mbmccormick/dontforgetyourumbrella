affe501b3007a45e2dcbae792e06c7d0bdabc281
Matt McCormick <mbmccormick@gmail.com>
2010-10-14 13:09:10 -0700

    Updated text casing.

--- index.php
+++ index.php
@@ -24,7 +24,7 @@
                     if (substr($cond, strlen($cond) - 1, 1) == "m")
                         $cond = $cond . "s";
                 }
-                echo "<span class='forecast'>" . $prefix . " <a title='Click here to view the probability of precipitation.' href='/probability'>" . $cond . "</a> with a high of " . $high . "&deg;F <a title='Click here to view the forecast for tomorrow.' href='/tomorrow'>today</a> in " . $city . ", " . $state . ". </span>\n";
+                echo "<span class='forecast'>" . $prefix . " <a title='Click here to view the probability of precipitation.' href='/probability'>" . strtolower($cond) . "</a> with a high of " . $high . "&deg;F <a title='Click here to view the forecast for tomorrow.' href='/tomorrow'>today</a> in " . $city . ", " . $state . ". </span>\n";
                 echo "<span class='umbrella'>Bring your <span class='yes'>umbrella</span>. </span><br />\n";
             }
             else
@@ -38,7 +38,7 @@
                 {
                     $prefix = "There will be a ";
                 }
-                echo "<span class='forecast'>" . $prefix . " <a title='Click here to view the probability of precipitation.' href='/probability'>" .  $cond . "</a> with a high of " . $high . "&deg;F <a title='Click here to view the forecast for tomorrow.' href='/tomorrow'>today</a> in " . $city . ", " . $state . ". </span>\n";
+                echo "<span class='forecast'>" . $prefix . " <a title='Click here to view the probability of precipitation.' href='/probability'>" .  strtolower($cond) . "</a> with a high of " . $high . "&deg;F <a title='Click here to view the forecast for tomorrow.' href='/tomorrow'>today</a> in " . $city . ", " . $state . ". </span>\n";
                 echo "<span class='umbrella'>Leave your <span class='no'>umbrella</span> at home. </span><br />\n";
             }	
             
